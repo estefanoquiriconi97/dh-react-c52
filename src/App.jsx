5e35364aaa085d8faa3ce5aec931e9495b4dcc3c
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-03-09 14:54:29 -0300

    fetch gifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Gif } from './assets/components/Gif'
+
+const URL_BASE = 'https://api.giphy.com/v1/gifs/trending'
+const API_KEY = '2G3plNGzf5KAERR1ekVJPak9Xo1yaLpv'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gifs, setGifs] = useState([])
+
+  const fetchApi = async () => {
+    const response = await fetch(`${URL_BASE}?api_key=${API_KEY}&limit=4`)
+    const data = await response.json()
+    setGifs(data.data)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
+        <div className='container'>
+          <a className='navbar-brand' href='#'>
+            GIPHY APP
+          </a>
+          <button
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarResponsive'
+            aria-controls='navbarResponsive'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span className='navbar-toggler-icon'></span>
+          </button>
+        </div>
+        <div className='collapse navbar-collapse' id='navbarResponsive'>
+          <ul className='nav ml-auto'>
+            <li className='nav-item'>
+              <button className='btn btn-success' onClick={fetchApi}>
+                Cargar random
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className='container'>
+        <div className='row text-center'>
+          {gifs.map((gif) => (
+            <Gif key={gif.id} gif={gif} />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/assets/components/Gif.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Gif = ({gif}) => {
+  return (
+    <div className='col-lg-3 col-md-6 mb-4'>
+      <div className='card h-100'>
+        <img className='card-img-top' src={gif.images.original.url} alt='' />
+        <div className='card-body'>
+          <h4 className='card-title'>{gif.title}</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
